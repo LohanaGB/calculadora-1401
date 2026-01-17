d66cf0842674e43c38af4076fd829179239fbfa9
Lohana <lohana.gvsp@gmail.com>
2026-01-17 16:44:02 -0300

    salvando estrutura básica

---
+++ public/styles/Global.styles.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const HomeContent = styled.div `
+	background: #fff;
+	color: #000;
+`;
+
+export const LayoutContent = styled.div `
+	color: red;
+	font-size: 20px;
+	font-family: Arial, Helvetica, sans-serif;
+`;
